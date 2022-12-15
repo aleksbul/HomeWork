eb9dac1bf7b3e6cc9604d0a5967ac93927694e28
aleksandra <alexandra.bulgak@gmail.com>
2022-12-15 12:00:04 +0200

    add Task 002 Max

---
+++ Task002_Max/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+if (B == A)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    if (B > A)
+    {
+        Console.WriteLine("Число " + B + " больше чем " + A);
+    }
+    else
+    {
+        Console.WriteLine("Число " + A + " больше чем " + B);
+    }
+}
