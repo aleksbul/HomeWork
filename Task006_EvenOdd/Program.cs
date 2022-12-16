efa64ef9cfc7d1be8e59e3dbacef528626e115ba
aleksandra <alexandra.bulgak@gmail.com>
2022-12-16 15:44:33 +0200

    add Task 006 Even or Odd

---
+++ Task006_EvenOdd/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число
+// и выдает, является ли число четным.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число " + number + " четное");
+}
+else 
+{
+    Console.WriteLine("Число " + number + " нечетное");
+}
