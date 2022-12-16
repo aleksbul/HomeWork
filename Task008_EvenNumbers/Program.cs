a5dcbaa279c7f1de524978d52b57df4e2da7dec8
aleksandra <alexandra.bulgak@gmail.com>
2022-12-16 17:25:13 +0200

    add Task 008 Even numbers from 1 to N

---
+++ Task008_EvenNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число N, 
+// а на выходе показывает все четные числа от 1 до N.
+
+Console.WriteLine("Введите число");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int i;
+int x;
+if(N > 0)
+{
+    i = 2;
+    x = 2;
+}
+else 
+{
+    i = 0;
+    x = -2;
+}
+for (; Math.Abs(i) < Math.Abs(N); i+=x)
+{
+    Console.Write(i + " ");
+}
